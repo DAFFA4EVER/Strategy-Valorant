83a6368502ffb7f42cebc3192a59d2449bf17dca
DAFFA4EVER <ihsbiasa@gmail.com>
2022-06-11 23:39:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -16,42 +16,6 @@ def dmg_calculation(weapon_info, criteria):
         n += 1
     return dmg_score
 
-def make_loadout(goal_loadout, budget, criteria, armor):
-    temp_loadout = []
-    armor_load = 'None'
-    armor_name = 'None'
-    for i in goal_loadout:
-        if(i == 'Anyweapon'):
-            for j in gun.shop[i]:
-                for k in j:
-                    if(k['Price'] <= budget):
-                        dmg_score = dmg_calculation(k, criteria)
-                        temp_loadout.append({'Name' : k['Name'], 'Weight': k['Price'], 'Value': dmg_score, 'Type': i})
-        elif(i == 'Armors'):
-            if(armor == 'Light'):
-                armor_load = {'Name' : gun.Armors[0]['Name'], 'Weight': gun.Armors[0]['Price'], 'Value': 1, 'Type': i}
-            elif(armor == 'Heavy'):
-                armor_load = {'Name' : gun.Armors[1]['Name'], 'Weight': gun.Armors[1]['Price'], 'Value': 2, 'Type': i}
-        else:
-            for j in gun.shop[i]:
-                if(j['Price'] <= budget):
-                    dmg_score = dmg_calculation(j, criteria)
-                    temp_loadout.append({'Name' : j['Name'], 'Weight': j['Price'], 'Value': dmg_score, 'Type': i})
-    
-    final_loadout = []
-    for g in temp_loadout:
-        weight = 0
-        value = 0
-        if(armor_load != 'None'):
-            weight = armor_load['Weight']
-            value = armor_load['Value']
-            armor_name = armor_load['Name']
-
-        weight = weight + g['Weight']
-        value = value + g['Value']
-        final_loadout.append({'Name':g['Name'],'Armor': armor_name,'Weight': weight,'Value': value})
-    return final_loadout
-
 if __name__ == "__main__":
     print('Rules : 0/1 Sidearms, 0/1 Main Weapon (SMGs, Rifles, Snipers, Heavies), 1 Armor (At least you have a Sidearms or Main Weapon')
     print('Catalog : Sidearms, SMGs, Shotguns, Rifles, Snipers, Heavies, Armors, Anyweapon\n')
@@ -89,7 +53,7 @@ if __name__ == "__main__":
         criteria = list(map(int, input().split()))
         algorithm = input("Greedy/Brute? ")
         if(algorithm == 'Brute'):
-            final = brute.brute_force(make_loadout(goal, money, criteria, armor_type), money)
+            final = brute.brute_force(brute.make_loadout(goal, money, criteria, armor_type), money)
         elif(algorithm == 'Greedy'):
             final = 'Blank'
         
